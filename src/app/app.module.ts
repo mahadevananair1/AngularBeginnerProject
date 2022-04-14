1f93d545dd7b53bc5a02d4ff246afe33a5c18e2e
mahadevananair1 <mahadevananair@gmail.com>
2022-04-14 21:40:55 +0530

    First commit of a Premature app Thank you github for keeping everything safe . Feeling so low again

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world-component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloWorldComponent,
+    HeaderComponent,
+    ButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { resolve } from 'dns';
+import { EventEmitter } from 'stream';
+
+
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+
+  @Input() text: string | undefined;
+  @Input() color: string | undefined;
+  @Output() btnClick = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick() {
+    console.log('Add')
+    this.btnClick.emit('');
+  }
+
+}
